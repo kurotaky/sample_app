605f9beec51a23bc9fced22dcd31f5519d38b232
Yuta Kurotaki <yuta.kurotaki@gmail.com>
2015-08-13 18:20:47 +0900

    ドキュメント書いてみた

--- app/controllers/api/microposts_controller.rb
+++ app/controllers/api/microposts_controller.rb
@@ -1,8 +1,8 @@
 class Api::MicropostsController < Api::ApplicationController
   def feed_items
     # @feed_items = current_user.feed
-    user = User.find_by(email: 'michael@example.com')
-    # user = User.first
+    # user = User.find_by(email: 'michael@example.com')
+    user = User.first
     @feed_items = user.microposts
   end
 end
